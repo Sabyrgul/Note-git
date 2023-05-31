a8bcd72a4e6d1af61023caa3943580f8ebac074e
Sabyrgul <quaker.ss11@gmail.com>
2023-06-01 01:42:18 +0600

    initial commit

---
+++ app/src/main/java/com/geektech/note_g_11/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.geektech.note_g_11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geektech.note_git.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
